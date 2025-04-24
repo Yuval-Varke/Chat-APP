04a5a941948c469cf61f6ee803c7ee5b116fd97a
Yuval-Varke <varke.vishu01@gmail.com>
2025-04-24 13:59:18 +0530

    comments added

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const http = require('http')
 const server = http.createServer(app);
 const io = new Server(server);
 const port = 4000;
-
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 
-io.on('connection',(socket)=>{
-    socket.on('send-name',(username)=>{
-        io.emit('send-name',username);
+io.on('connection',(socket)=>{   //what this line does is that it listens for a connection event from the client and when the client connects, it executes the callback function.
+    socket.on('send-name',(username)=>{ //this line listens for the send-name event from the client and when it receives the event, it executes the callback function.
+        io.emit('send-name',username); //this line emits the send-name event to all connected clients with the username as data.
     });
 
     socket.on('send-message',(message)=>{
